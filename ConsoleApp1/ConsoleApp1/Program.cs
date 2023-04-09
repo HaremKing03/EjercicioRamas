6977dad73217008febfb67343e8ed8e7a6cf59c0
martin <moreiramartin232@gmail.com>
2023-04-09 20:59:50 -0300

    Commit-3 exepciones

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,40 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            Random numero = new Random();
+            int aleatorio = numero.Next(0, 100);
+
+            int minumero;
+            int intentos = 0;
+            Console.WriteLine("introduce un numero entre 0 y 100");
+
+            do
+            {
+                intentos++;
+                try
+                {
+                    minumero = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("No es un numero Exeption");
+                    minumero = 0;
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("El numero es demasiado alto overflow");
+                    minumero = 0;
+                }
+
+                if (minumero > aleatorio) Console.WriteLine("El numero es mas Bajo");
+                if (minumero < aleatorio) Console.WriteLine("El numero es mas Alto");
+
+
+            } while (aleatorio != minumero);
+
+            Console.WriteLine($"CORRECTO HAS NECESITADO {intentos} intentos");
+
         }
     }
 }
