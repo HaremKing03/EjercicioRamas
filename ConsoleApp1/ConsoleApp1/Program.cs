4fdd8452eccd79d1b5064574718f23ac81a01216
martin <moreiramartin232@gmail.com>
2023-04-09 21:16:44 -0300

    Commit-4 exepciones

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            Random numero = new Random();
+            int aleatorio = numero.Next(0, 100);
+
+            int minumero;
+            int intentos = 0;
+            Console.WriteLine("introduce un numero entre 0 y 100");
+
+            do
+            {
+                intentos++;
+                try
+                {
+                    minumero = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("No es un numero Exeption");
+                    Console.WriteLine(ex.Message);
+                    minumero = 0;
+                }
+           
+
+                if (minumero > aleatorio) Console.WriteLine("El numero es mas Bajo");
+                if (minumero < aleatorio) Console.WriteLine("El numero es mas Alto");
+
+
+            } while (aleatorio != minumero);
+
+            Console.WriteLine($"CORRECTO HAS NECESITADO {intentos} intentos");
+
         }
     }
 }
